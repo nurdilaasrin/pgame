a2da871f1c27a9ea01828871e5c87392a7032e6e
nurdilaasrin <56948248+nurdilaasrin@users.noreply.github.com>
2019-10-24 11:00:17 +0700

    tugas3

---
+++ pgame-16017/code_tugas3.py
@@ -0,0 +1,9 @@
+# Latihan Buat Variabel : 
+nama = "Nama Saya 	: Nurdila Asrin" 
+print (nama)
+npm = "NPM 		: 121055520116017" 
+print (npm)
+semester = "Semester 	: 7 (Tujuh)" 
+print (semester)
+matakuliah = "Mata Kuliah 	: Pemograman Game" 
+print (matakuliah)
