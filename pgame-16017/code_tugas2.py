b65c92afbaa7e22eec441ebb8cda0568e9a3b40d
nurdilaasrin <56948248+nurdilaasrin@users.noreply.github.com>
2019-10-24 10:54:43 +0700

    tugas2

---
+++ pgame-16017/code_tugas2.py
@@ -0,0 +1,6 @@
+
+print ("Hello mahasiswa muhammadiyah")
+print ("Saya mau berusaha")
+#baris ini tidak akan dieksekusi
+print ("Halo anak komputasi") #baris ini dieksekusi
+#Baris ini juga merupakan Baris Komentar 
