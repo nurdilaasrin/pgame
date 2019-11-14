78eda3a9f73bacde2f5ad19a055c479ff5c6d93c
nurdilaasrin <56948248+nurdilaasrin@users.noreply.github.com>
2019-11-14 19:56:30 +0700

    LATIHAN - Praktikum -4

---
+++ pgame-16017/code41.py
@@ -0,0 +1,22 @@
+# Pemrograman Game Praktikum 4
+# latihan code 4.1 : Numerik (integer dan float)
+# buat variabel dan beri nilai
+a = 4
+b = 5
+c = 6.3
+# cetak variabel ke layar
+print(a)
+print(b)
+print(c)
+# cetak '*' sebanyak 35 kali
+print('*'*35)
+# cetak variabel dengan keterangan ke layar
+print("Variable A :", a )
+print("Variable B : %d " % b )
+print("Variable C : %f " % c )
+print('*'*35)
+# cetak variabel dengan keterangan ke laya
+# operasi + , * , / dan cetak ke layar
+print("Variable A+B : ", a+b )
+print("Variable A*B : ", a*b )
+print("Variable C/3 : ", c/3 )
